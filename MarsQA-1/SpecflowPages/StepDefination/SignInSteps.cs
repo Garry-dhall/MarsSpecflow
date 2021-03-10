06f2db8073069441e3de17fad1cb4f0c0b85c3e9
Garry <gdhall100@gmail.com>
2021-03-10 23:43:24 +1300

    Signin

--- MarsQA-1/SpecflowPages/StepDefination/SignInSteps.cs
+++ MarsQA-1/SpecflowPages/StepDefination/SignInSteps.cs
@@ -1,39 +1,49 @@
-﻿using System;
+﻿using MarsQA_1.Helpers;
+using MarsQA_1.Pages;
+using System;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
 namespace MarsQA_1.Feature
 {
     [Binding]
     public class SignInSteps
+
     {
+
         [Given(@"I am on homepage")]
         public void GivenIAmOnHomepage()
         {
-            ScenarioContext.Current.Pending();
+            Driver.TurnOnWait();
+            Driver.Initialize();
+            Driver.NavigateUrl();
         }
         
         [When(@"I click on SignIn button")]
         public void WhenIClickOnSignInButton()
         {
-            ScenarioContext.Current.Pending();
+            Driver.TurnOnWait();
+            SignIn.ClickSignInBtn();
         }
         
         [When(@"I enter valid credentials")]
         public void WhenIEnterValidCredentials(Table table)
         {
-            ScenarioContext.Current.Pending();
+            //dynamic tableDetails = table.
+
         }
         
         [When(@"I click on Login Button")]
         public void WhenIClickOnLoginButton()
         {
-            ScenarioContext.Current.Pending();
+            Driver.TurnOnWait();
+            SignIn.ClickOnLoginBtn();
         }
         
         [Then(@"I should be able to login successfully")]
         public void ThenIShouldBeAbleToLoginSuccessfully()
         {
-            ScenarioContext.Current.Pending();
+           
         }
     }
 }
